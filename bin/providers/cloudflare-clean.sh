58c1a732a599812acf0ec8859f6bb554368ab500
Etienne Sellan <git@sellan.fr>
2025-09-07 00:24:51 +0200

    Fix cloudflare cleanup

--- bin/providers/cloudflare-clean.sh
+++ bin/providers/cloudflare-clean.sh
@@ -9,10 +9,12 @@ zone_id=$(curl -s -X GET "$api/zones?name=$domain" \
                   -H "Authorization: Bearer $API_KEY" \
                   -H "Content-Type: application/json" | jq -r '.result[0].id' )
 
-record_id=$(curl -s -X GET "$api/zones/$zone_id/dns_records?type=TXT&name=$record_name.$domain" \
-                    -H "Authorization: Bearer $API_KEY" \
-                    -H "Content-Type: application/json" | jq -r '.result[0].id' )
+record_ids=$(curl -s -X GET "$api/zones/$zone_id/dns_records?type=TXT&name=$record_name.$domain" \
+                     -H "Authorization: Bearer $API_KEY" \
+                     -H "Content-Type: application/json" | jq -r '.result[].id')
 
-curl -s -X DELETE  "$api/zones/$zone_id/dns_records/$record_id" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $API_KEY"
+for rid in $record_ids; do
+        curl -s -X DELETE "$api/zones/$zone_id/dns_records/$rid" \
+                -H "Content-Type: application/json" \
+                -H "Authorization: Bearer $API_KEY"
+done
